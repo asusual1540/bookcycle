5b0c452c9e5828229d3897612ce53735676a4550
Adnan <mehedihasanadnan@gmail.com>
2018-11-01 01:37:09 +0600

    Delete unnecessary files

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const _ = require("lodash");
 const multer = require("multer");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-
 //=======================================STARTING-EXPRESS-APP======================//
 const app = express();
 var port = process.env.PORT || 3000;
@@ -59,17 +58,11 @@ function isLoggedIn(req, res, next) {
 }
 
 // this middle ware returns user to every routes
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.locals.currentUser = req.user;
   next();
 });
 
-app.use(function(err, req, res, next) {
-  if (err) {
-    res.locals.error = err.message;
-  }
-  next();
-});
 
 //===============================================GET-ROUTES==========================//
 app.get("/", (req, res) => {
@@ -98,9 +91,7 @@ app.get(
   }
 );
 
-app.get("/signup", (req, res) => {
-  res.render("signup.ejs", {});
-});
+
 
 // Show all books collections
 app.get(
@@ -114,9 +105,6 @@ app.get(
     res.status(400).send(err);
   }
 );
-app.get("/demo", (req, res) => {
-  res.send('<script>alert("Hello")</script>');
-});
 
 //show all Users collection
 app.get(
@@ -175,6 +163,12 @@ app.get("/logout", (req, res) => {
   res.redirect("/");
 });
 
+app.get("/signup", (req, res) => {
+  res.render("signup.ejs", {
+
+  })
+})
+
 //===================================================POST-ROUTES===========================//
 app.post(
   "/sell",
@@ -214,141 +208,35 @@ app.post(
   }
 );
 
-app.post("/signup", (req, res) => {
-  var newUser = new User({ username: req.body.username });
-  User.register(newUser, req.body.password, (err, user) => {
-    if (err) {
-      res.status(400).send(err.message);
-      return res.render("signup.ejs");
-    }
-    passport.authenticate("local")(req, res, function() {
-      res.redirect("/");
-    });
-  });
-});
+app.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ email: 'Email already exists' })
+      } else {
+        const newUser = new User({
+          username: req.body.username,
+          email: req.body.email
+        })
+
+        User.register(newUser, req.body.password, (err, user) => {
+          if (err) {
+            res.status(400).send(err.message)
+            return res.render("signup.ejs")
+          }
+          passport.authenticate("local")(req, res, function () {
+            res.redirect("/")
+          })
+        })
+      }
+    })
+})
+
+//=============================================USE ROUTES================================//
+
 
 //=============================================STARTING-APP==============================//
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-// logger
-// app.use((req, res, next) => {
-//     var now = new Date().toString()
-//     var log = `${now}: ${req.method} ${req.url} ${req.ip}`
-//     console.log(log)
-//     fs.appendFile('server.log', log + '\n', (err) => {
-//         if(err) {
-//             console.log('Unable to append to server.log')
-//         }
-//     })
-//     next()
-// })
-
-// app.use((req, res, next) => {
-//     res.render('maintenance.hbs', {
-//         pageTitle: 'Site on Maintenance',
-//         currentYear: new Date().getFullYear()
-//     })
-// })
-
-//update a single book properties.. should be a private route
-// app.patch('/books/:id', (req, res) => {
-//     var id = req.params.id
-//     var body = _.pick(req.body, ['name', 'author', 'price'])
-
-//     if (!ObjectID.isValid(id)) {
-//       return res.status(404).send('Invalid id')
-//     }
-
-//     Book.findByIdAndUpdate(id, {$set: body}, {new: true}).then((docs) => {
-//       if (!docs) {
-//         return res.status(404).send('retry')
-//       }
-//       res.send({docs});
-//     }).catch((e) => {
-//       res.status(400).send(e)
-//     })
-//   })
-
-// a private route which implements authenticate function
-// app.get('/users/me', authenticate, (req, res) => {
-//     res.send(req.user)
-// })
-
-// Submitting a book by user
-// app.post('/books', (req, res) => {
-//     var book = new Book({
-//         name: req.body.name,
-//         author: req.body.author
-//     })
-//     book.save().then((result) => {
-//         res.send(result)
-//     }, (e) => {
-//         console.log(e)
-//     })
-// })
-
-// Submitting info by a user by user like signing up...should be a private route
-// app.post('/users/signup', (req, res) => {
-//     var body = _.pick(req.body, ['name', 'email', 'password'])
-//     if (body.name && body.email && body.password) {
-//         var user = new User(body)
-//     } else {
-//         console.log('error occured')
-//     }
-//     // model method (should be a custom method)
-//     // User.findByToken
-
-//     // instance method
-//     // user.generateAuthToken
-
-//     user.save().then(() => {
-//         return user.generateAuthToken()
-//     }).then((token) => {
-//         res.header('x-auth', token).send(`You are a new user. with id: ${user.id}, name: ${user.name}, password:${user.password}`)
-//     }).catch ((e) => {
-//         res.send(e)
-//     })
-// })
-
-// POST /users/login {email, password}
-// app.post('/users/login', (req, res) => {
-//     var body = _.pick(req.body, ['name', 'email', 'password']);
-
-//     User.findByCredentials(body.email, body.password).then((user) => {
-//         return user.generateAuthToken().then((token) => {
-//             console.log(`Token generated: ${token}`)
-//             res.header('x-auth', token).send(user);
-//           });
-//     }).catch((e) => {
-//       res.status(400).send()
-//       console.log(e)
-//     });
-//   });
-
-// app.get('/categories', (req, res) => {
-//     res.render('categories.hbs', {
-//         // pageTitle: 'About Page',
-//         // currentYear: new Date().getFullYear()
-//     })
-// })
-
-// app.post('/addUser', (req, res) => {
-//     var newUsername = req.body.newUsername
-//     var newEmail = req.body.newEmail
-//     var newPassword = req.body.newPassword
-//     var newUser = new User({
-//         name: newUsername,
-//         email: newEmail,
-//         password: newPassword
-//     })
-//     User.create(newUser, (err, freshUser) => {
-//         if (err) {
-//             console.log(err)
-//         } else {
-//             res.redirect('/signup')
-//             console.log(freshUser)
-//         }
-//     })
-// })
--- server/models/user.js
+++ server/models/user.js
@@ -8,87 +8,26 @@ var UserSchema = new mongoose.Schema({
     username: {
         type: String,
         requierd: true,
-        trim: true, 
+        trim: true,
         minlength: 1
-    }, password: {
+    },
+    email: {
         type: String,
-        require: true,
-        minlength: 4,
+        requierd: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
 })
 
 UserSchema.plugin(passportLocalMongoose)
-
-// function for sending only id and username property to user
-// UserSchema.methods.toJSON = function () {
-//     var user = this
-//     var userObject = user.toObject()
-
-//     return _.pick(userObject, ['_id', 'username'])
-// }
-
-// instance authentication token generating function using jsonwebtoken
-// UserSchema.methods.generateAuthToken = function () {
-//     var user = this
-//     var access = 'auth'
-
-//     // generate auth token
-//     var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString()
-
-//     // passing user token property
-//     user.tokens.push({
-//         access,
-//         token
-//     })
-//     //returning token after saving a user
-//     return user.save().then(() => {
-//         return token
-//     })
-// }
-
-// model method
-// UserSchema.statics.findByToken = function (token) {
-//     var User = this
-//     var decoded
-
-//     try {
-//         decoded = jwt.verify(token, 'abc123')
-//     } catch (e) {
-//         // return new Promise((resolve, reject) => {
-//         //     reject()
-//         // })
-//         return Promise.reject()
-//     }
-//     return User.findOne({
-//         '_id': decoded._id,
-//         'tokens.token': token,
-//         'tokens.access': 'auth'
-//     })
-// }
-
-// UserSchema.statics.findByCredentials = function (email, password) {
-//     var User = this
-
-//     return User.findOne({email}).then((user) => {
-//       if (!user) {
-//         return Promise.reject()
-//       }
-//       console.log('found user.. now verifying password')
-//       console.log(`Pasword provided by user is: ${password}`)
-//       console.log(`Password stored in database is: ${user.password}`)
-//       return Promise((resolve, reject) => {
-//         // bcrypt.compare to compare password and user.password
-//         bcrypt.compare(password, user.password, (err, res) => {
-//           if (res) {
-//             resolve(user)
-//           } else {
-//               console.log(err)
-//             reject()
-//           }
-//         })
-//       })
-//     })
-//   }
 
 var User = mongoose.model('User', UserSchema)
 module.exports = {
