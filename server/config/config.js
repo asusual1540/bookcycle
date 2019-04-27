dc45ae5608741dabdaf53c68e2bfa43e637756eb
asusual1540 <mehedihasanadnan@gmail.com>
2019-04-27 11:17:52 +0600

    secured fb api google api key

--- server/config/config.js
+++ server/config/config.js
@@ -2,15 +2,15 @@ module.exports = {
     "mongoURI": "mongodb://adnan:adnan1540@ds141813.mlab.com:41813/bookcycle",
     "secretKey": "Iamkira1540",
     'facebookAuth': {
-        'clientID': '2113469788922621', // your App ID
-        'clientSecret': 'b0481c6222414679e87988a85193f843', // your App Secret
+        'clientID': '', // your App ID
+        'clientSecret': '', // your App Secret
         'callbackURL': '/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields': ['id', 'email', 'name', 'picture.type(large)'] // For requesting permissions from Facebook API
     },
     'googleAuth': {
-        'clientID': '173867288656-dhohd4plhrr4tu8ne79mb9fqmj3ejsiq.apps.googleusercontent.com',
-        'clientSecret': 'BEy-OHR9Ee9k2Wfd1TUwX8O8',
+        'clientID': '',
+        'clientSecret': '',
         'callbackURL': '/auth/google/callback'
     }
 }
